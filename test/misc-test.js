4cd5710ebe090ec7c2d735840efb453e9a5a14bd
RedSparr0w <dan-jmn@hotmail.com>
2016-02-08 14:20:20 +1300

    attempt to shorten test time

--- test/misc-test.js
+++ test/misc-test.js
@@ -1,7 +1,7 @@
 'use strict';
 /* jshint node: true, mocha:true */
 
-require('../lib/miscHelper');
+require('../src/miscHelper');
 var assert = require('chai').assert;
 
 describe('CSGOParser Misc', function(){
