ee4384d5ceeefc9a3606024a396b25ed095995fb
sentisso <sentisso.dev@protonmail.com>
2023-01-29 01:40:17 +0100

    fixing missing dirs

--- example.js
+++ example.js
@@ -3,13 +3,20 @@
 
 const fs = require('fs'),
 	parser = require('./lib/csgo-data-parser'),
-	misc = require('./lib/miscHelper');
+	misc = require('./lib/miscHelper'),
+	path = require('path');
 
 const schemaFilePath = './test/test-data/schema.txt',
 	langFilePath = './test/test-data/csgo_english.txt',
 	itemsFilePath = './test/test-data/items_game.txt',
 	outDataFilePath = './out/data_' + Date.now() + '.json',
 	outLogFilePath = './out/logs/parser.log';
+
+// create the output log directory if it doesn't exist
+const outLogDir = path.dirname(outLogFilePath)
+if (!fs.existsSync(outLogDir)){
+	fs.mkdirSync(outLogDir, { recursive: true });
+}
 
 const csgoDataParser = new parser(schemaFilePath, langFilePath, itemsFilePath, 'debug', outLogFilePath);
 
@@ -33,6 +40,12 @@ csgoDataParser.getLogger().info('-----------------------------------------');
 csgoDataParser.getLogger().info('');
 csgoDataParser.getLogger().info('End Generations [' + misc.resultTimer(timer) +'s]');
 
+// create the output data directory if it doesn't exist
+const outDataDir = path.dirname(outDataFilePath)
+if (!fs.existsSync(outDataDir)){
+	fs.mkdirSync(outDataDir, { recursive: true });
+}
+
 const fd = fs.openSync(outDataFilePath, 'w');
-fs.writeSync(fd, JSON.stringify(infos,null,4));
+fs.writeSync(fd, JSON.stringify(infos));
 fs.closeSync(fd);
